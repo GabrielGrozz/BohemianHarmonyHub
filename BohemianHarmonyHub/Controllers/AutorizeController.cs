14696800dfdc1b65df6e2756fccfe26d580f7e78
GabrielGrozz <gabrielgrozz.net@gmail.com>
2024-03-04 17:46:12 -0300

    started identity

---
+++ BohemianHarmonyHub/Controllers/AutorizeController.cs
@@ -0,0 +1,71 @@
+﻿using BohemianHarmonyHub.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BohemianHarmonyHub.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AutorizeController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AutorizeController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "AutorizeController :: Acessado em:" + DateTime.Now.ToString();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser([FromBody] UserDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values.SelectMany(e => e.Errors));
+            }
+
+            var user = new IdentityUser { UserName = model.Email, Email = model.Email, EmailConfirmed = true };
+
+            var result = await _userManager.CreateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            await _signInManager.SignInAsync(user, false);
+
+            return Ok("usuario criado com sucessso!");
+        }
+
+        [HttpPost("login")]
+        public  async Task<ActionResult> Login([FromBody]UserDTO userInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values.SelectMany(e => e.Errors));
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(userInfo.Email, userInfo.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Login Inválido!");
+                return BadRequest(ModelState);
+            }
+
+        }
+    }
+}
